b481c9611b18b31ea703fae53cdf6758443814ee
TamniVilajet <ermin.kirlic@gmail.com>
2022-04-12 19:54:50 +0200

    Exercise 06

---
+++ ex06/src/index.js
@@ -0,0 +1,9 @@
+function firstName(firstName) {
+    return firstName.toUpperCase();
+}
+
+function lastName(lastName) {
+    return lastName.toLowerCase()
+}
+console.log(firstName("ermin"));
+console.log(lastName("ERMIN"));
